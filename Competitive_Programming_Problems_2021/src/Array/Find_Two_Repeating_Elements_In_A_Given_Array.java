21d8eafbc5f69024ad01c101fac8e06888276daa
shubham <shubmnagrare@gmail.com>
2021-05-25 20:40:04 +0530

    Array Problem

---
+++ Competitive_Programming_Problems_2021/src/Array/Find_Two_Repeating_Elements_In_A_Given_Array.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class Find_Two_Repeating_Elements_In_A_Given_Array {
+	
+	// https://www.geeksforgeeks.org/find-the-two-repeating-elements-in-a-given-array/
+	
+	public void solve() {
+		
+		int[] arr = {4, 2, 4, 5, 2, 3, 1};			// Answer  - 4 2
+		int n=5;
+		
+	}
+
+}
